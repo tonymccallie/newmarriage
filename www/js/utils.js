2bd8e0b8bcb52bc52a54d56b18e41d5a91cf777f
Tony McCallie <tony@threeleaf.net>
2016-02-01 16:16:06 -0600

    Allow empty json for new user login

--- www/js/utils.js
+++ www/js/utils.js
@@ -54,7 +54,7 @@ angular.module('greyback.utils', [])
 			return JSON.stringify(obj);
 		},
 		toObj: function (json) {
-			return JSON.parse(json);
+			return JSON.parse(json || "{}");
 		},
 	}
 })
