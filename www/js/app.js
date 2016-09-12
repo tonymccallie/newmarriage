e8923a61c4b6d86f53e4f0fc8a2fda7bf248a43c
Tony McCallie <tony@threeleaf.net>
2016-09-12 15:12:22 -0500

    removed kyeboard plugin and the test in app.js

--- www/js/app.js
+++ www/js/app.js
@@ -34,10 +34,15 @@ angular.module('greyback', ['ionic', 'ngCordova', 'ImgCache', 'ionic.service.cor
 		$ionicAnalytics.register();
 		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
 		// for form inputs)
-		if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+//		if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+//			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+//			$cordovaSplashscreen.hide();
+//		}
+//		
+		if (window.cordova && window.cordova.plugins) {
 			$cordovaSplashscreen.hide();
 		}
+		
 		if (window.StatusBar) {
 			// org.apache.cordova.statusbar required
 			$cordovaStatusBar.style(2);
