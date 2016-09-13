b6404b5e1993f42549e1972d7d34b352bd227821
Tony McCallie <tony@threeleaf.net>
2016-09-13 13:34:56 -0500

    updated plugin link?

--- www/js/app.js
+++ www/js/app.js
@@ -34,15 +34,11 @@ angular.module('greyback', ['ionic', 'ngCordova', 'ImgCache', 'ionic.service.cor
 		$ionicAnalytics.register();
 		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
 		// for form inputs)
-//		if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-//			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-//			$cordovaSplashscreen.hide();
-//		}
-//		
-		if (window.cordova && window.cordova.plugins) {
+		if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 			$cordovaSplashscreen.hide();
 		}
-		
+
 		if (window.StatusBar) {
 			// org.apache.cordova.statusbar required
 			$cordovaStatusBar.style(2);
